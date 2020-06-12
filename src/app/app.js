e287c47dc5fc8ae90e890ac73640a932430a9ffd
szymkac <szymek1295@gmail.com>
2020-06-12 22:32:07 +0200

    Working keep ratio mode

--- src/app/app.js
+++ src/app/app.js
@@ -6,8 +6,8 @@ const App = () => {
   return (
     <main>
       <ResizerWrapper>
-        <Resizable left={'100px'} top={'100px'} keepRatio>
-          <img src='https://pbs.twimg.com/profile_images/619691410992672768/TwSZQOYf_400x400.png' />
+        <Resizable left={'100px'} top={'100px'} allowOverflow={false}>
+          <img src='https://pbs.twimg.com/profile_images/619691410992672768/TwSZQOYf_400x400.png' alt='pepeusz' />
         </Resizable>
       </ResizerWrapper>
     </main>
--- src/app/components/resizer/resizable.js
+++ src/app/components/resizer/resizable.js
@@ -17,88 +17,85 @@ const dimensionHelpers = {
     horizontal: { size: 'width', cursor: 'pageX', pos: 'left', oppositeSize: 'height' },
 }
 
-const handleResize = (onMoveEvent, element, anchor) => {
-    const style = element.style;
+const calculateNewSize = (dir, element, onMoveEvent, allowOverflow) => {
     const boundingClientRect = element.getBoundingClientRect();
     const parentBoundingClientRect = element.parentElement.getBoundingClientRect();
+    const { sizeFactor, deltaFactor, changePos, dimension } = resizeFactors[dir];
+    const { size, cursor, pos, oppositeSize } = dimensionHelpers[dimension];
 
+    const overflow = !(onMoveEvent[cursor] > parentPadding && onMoveEvent[cursor] < parentBoundingClientRect[size] - parentPadding);
+    const newSize = !allowOverflow && overflow ? boundingClientRect[size] :
+        (boundingClientRect[size] * sizeFactor) + ((boundingClientRect[pos] - onMoveEvent[cursor]) * deltaFactor);
+
+    if (newSize > minSize) {
+        const newPos = changePos && onMoveEvent[cursor] > parentPadding ? onMoveEvent[cursor] : boundingClientRect[pos];
+        return { newSize, newPos, oppositeSize, size, pos, sizeChanges: newSize !== boundingClientRect[size] };
+    }
+    else {
+        return { newSize: boundingClientRect[size], newPos: boundingClientRect[pos], oppositeSize, size, pos, sizeChanges: false };
+    }
+}
+
+const handleResize = (onMoveEvent, element, anchor, allowOverflow) => {
+    const style = element.style;
     const diagonal = anchor.length > 1;
-    anchor.split('').forEach(a => {
-        const { sizeFactor, deltaFactor, changePos, dimension } = resizeFactors[a];
-        const { size, cursor, pos, oppositeSize } = dimensionHelpers[dimension];
 
-        if (onMoveEvent[cursor] > parentPadding && onMoveEvent[cursor] < parentBoundingClientRect[size] - parentPadding) {
-            const newSize = (boundingClientRect[size] * sizeFactor) + ((boundingClientRect[pos] - onMoveEvent[cursor]) * deltaFactor);
-
-            if (newSize > minSize) {
-                style[size] = newSize + 'px';
-                if (!diagonal && !style[oppositeSize]) {
-                    style[oppositeSize] = boundingClientRect[oppositeSize] + 'px';
-                }
-                if (changePos) {
-                    style[pos] = onMoveEvent[cursor] + 'px';
-                }
-            }
+    anchor.split('').forEach(dir => {
+        const { newSize, newPos, oppositeSize, size, pos } = calculateNewSize(dir, element, onMoveEvent, allowOverflow);
+        style[size] = newSize + 'px';
+        style[pos] = newPos + 'px';
+        if (!diagonal && !style[oppositeSize]) {
+            style[oppositeSize] = element.getBoundingClientRect()[oppositeSize] + 'px';
         }
     })
 }
 
-const calculateNewSize = (dir, element, onMoveEvent, allowOverflow = true) => {
-    const boundingClientRect = element.getBoundingClientRect();
-    const parentBoundingClientRect = element.parentElement.getBoundingClientRect();
-    const { sizeFactor, deltaFactor, changePos, dimension } = resizeFactors[dir];
-    const { size, cursor, pos } = dimensionHelpers[dimension];
-
-    const overflow = onMoveEvent[cursor] > parentPadding && onMoveEvent[cursor] < parentBoundingClientRect[size] - parentPadding;
-    const newSize = !allowOverflow && overflow ? boundingClientRect[size] :
-        (boundingClientRect[size] * sizeFactor) + ((boundingClientRect[pos] - onMoveEvent[cursor]) * deltaFactor);
-
-    return { overflow, newSize, changePos, cursor };
-}
-
-const handleResizeWithRatio = (onMoveEvent, element, anchor) => {
+const handleResizeWithRatio = (onMoveEvent, element, anchor, allowOverflow) => {
     const style = element.style;
     const boundingClientRect = element.getBoundingClientRect();
     const parentBoundingClientRect = element.parentElement.getBoundingClientRect();
     const { height: oldHeight, width: oldWidth } = boundingClientRect;
     const [verticalDir, horizontalDir] = anchor.split('');
 
-    const { newSize: newHeight, changePos: changeTop } = calculateNewSize(verticalDir, element, onMoveEvent);
-    const newWidth = oldWidth * newHeight / oldHeight;
+    const { newSize: newHeight, newPos: newTop, sizeChanges: heightChange } = calculateNewSize(verticalDir, element, onMoveEvent, allowOverflow);
+    if (heightChange) {
+        const newWidth = oldWidth * newHeight / oldHeight;
+        const newLeft = resizeFactors[horizontalDir].changePos ? boundingClientRect.left + oldWidth - newWidth : boundingClientRect.left;
+        const overflowRight = parentBoundingClientRect.width - parentPadding < newWidth + newLeft;
 
-    if (newHeight > minSize && newWidth > minSize) {
-        style.width = newWidth + 'px';
-        style.height = newHeight + 'px';
-        if(changeTop){
-            style.top = onMoveEvent.pageY + 'px';
+        if (newWidth > minSize && newLeft > parentPadding && !overflowRight) {
+            style.width = newWidth + 'px';
+            style.height = newHeight + 'px';
+            style.top = newTop + 'px';
+            style.left = newLeft + 'px';
         }
     }
 }
 
-const Resizable = ({ children, keepRatio, ...styleProps }) => {
+const Resizable = ({ children, keepRatio, allowOverflow = false, ...styleProps }) => {
     const resizableRef = useRef();
 
     const onStartResize = onMouseDownEvent => {
         onMouseDownEvent.preventDefault()
         const { parentElement } = resizableRef.current;
         parentElement.addEventListener('mousemove', onResize);
-        parentElement.addEventListener('mouseup', onStopResize)
-        parentElement.addEventListener('mouseleave', onStopResize)
+        parentElement.addEventListener('mouseup', onStopResize);
+        parentElement.addEventListener('mouseleave', onStopResize);
 
         const anchor = onMouseDownEvent.target.getAttribute('name');
 
         function onResize(onMoveEvent) {
             if (keepRatio) {
-                handleResizeWithRatio(onMoveEvent, resizableRef.current, anchor)
+                handleResizeWithRatio(onMoveEvent, resizableRef.current, anchor, allowOverflow)
             }
             else {
-                handleResize(onMoveEvent, resizableRef.current, anchor);
+                handleResize(onMoveEvent, resizableRef.current, anchor, allowOverflow);
             }
         };
 
         function onStopResize() {
             parentElement.removeEventListener('mousemove', onResize);
-            parentElement.removeEventListener('mouseleave', onStopResize)
+            parentElement.removeEventListener('mouseleave', onStopResize);
             parentElement.removeEventListener('mouseup', onStopResize);
         };
     };
